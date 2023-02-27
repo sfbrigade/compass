473c803884ff132ca93ce2c0b65b8f500f1ab7fd
Anup Mantri <anup.mantri@gmail.com>
2023-02-27 14:58:07 -0800

    Run 'npm install' on init to ensure that modules and deps are up to date. (#19)

--- bin/init.sh
+++ bin/init.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-# wait for postgres to accept connections...
+# Wait for postgres to accept connections...
 while !</dev/tcp/database/5432; do
   sleep 1
 done
 
-# check if database exists- if not, create and run migrations and seeds
+# Check if database exists- if not, create and run migrations and seeds
 psql ${DATABASE_URL} -c '' > /dev/null 2>&1
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   npm run db:reset -w server
 fi
+
+# Make sure npm modules are up to date.
+npm install
