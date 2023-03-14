948072f9eab6715f1b5da0ee241c47378581a64b
amantri <anup.mantri@gmail.com>
2023-03-13 19:55:55 -0700

    .

--- proto/gen/compass_grpc_pb.js
+++ proto/gen/compass_grpc_pb.js
@@ -1,38 +1,35 @@
 // GENERATED CODE -- DO NOT EDIT!
 
-"use strict";
-var grpc = require("@grpc/grpc-js");
-var compass_pb = require("./compass_pb.js");
+'use strict';
+var grpc = require('@grpc/grpc-js');
+var compass_pb = require('./compass_pb.js');
 
 function serialize_compass_GetStudentsRequest(arg) {
   if (!(arg instanceof compass_pb.GetStudentsRequest)) {
-    throw new Error("Expected argument of type compass.GetStudentsRequest");
+    throw new Error('Expected argument of type compass.GetStudentsRequest');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_compass_GetStudentsRequest(buffer_arg) {
-  return compass_pb.GetStudentsRequest.deserializeBinary(
-    new Uint8Array(buffer_arg)
-  );
+  return compass_pb.GetStudentsRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
 function serialize_compass_GetStudentsResponse(arg) {
   if (!(arg instanceof compass_pb.GetStudentsResponse)) {
-    throw new Error("Expected argument of type compass.GetStudentsResponse");
+    throw new Error('Expected argument of type compass.GetStudentsResponse');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_compass_GetStudentsResponse(buffer_arg) {
-  return compass_pb.GetStudentsResponse.deserializeBinary(
-    new Uint8Array(buffer_arg)
-  );
+  return compass_pb.GetStudentsResponse.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-var CompassService = (exports.CompassService = {
+
+var CompassService = exports.CompassService = {
   getStudents: {
-    path: "/compass.Compass/GetStudents",
+    path: '/compass.Compass/GetStudents',
     requestStream: false,
     responseStream: false,
     requestType: compass_pb.GetStudentsRequest,
@@ -42,6 +39,6 @@ var CompassService = (exports.CompassService = {
     responseSerialize: serialize_compass_GetStudentsResponse,
     responseDeserialize: deserialize_compass_GetStudentsResponse,
   },
-});
+};
 
 exports.CompassClient = grpc.makeGenericClientConstructor(CompassService);
--- proto/gen/compass_pb.d.ts
+++ proto/gen/compass_pb.d.ts
@@ -6,98 +6,70 @@
 
 import * as jspb from "google-protobuf";
 
-export class Student extends jspb.Message {
-  getId(): string;
-  setId(value: string): Student;
-  getFirstName(): string;
-  setFirstName(value: string): Student;
-  getLastName(): string;
-  setLastName(value: string): Student;
-  getEmail(): string;
-  setEmail(value: string): Student;
+export class Student extends jspb.Message { 
+    getId(): string;
+    setId(value: string): Student;
+    getFirstName(): string;
+    setFirstName(value: string): Student;
+    getLastName(): string;
+    setLastName(value: string): Student;
+    getEmail(): string;
+    setEmail(value: string): Student;
 
-  serializeBinary(): Uint8Array;
-  toObject(includeInstance?: boolean): Student.AsObject;
-  static toObject(includeInstance: boolean, msg: Student): Student.AsObject;
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
-  static extensionsBinary: {
-    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
-  };
-  static serializeBinaryToWriter(
-    message: Student,
-    writer: jspb.BinaryWriter
-  ): void;
-  static deserializeBinary(bytes: Uint8Array): Student;
-  static deserializeBinaryFromReader(
-    message: Student,
-    reader: jspb.BinaryReader
-  ): Student;
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): Student.AsObject;
+    static toObject(includeInstance: boolean, msg: Student): Student.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: Student, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): Student;
+    static deserializeBinaryFromReader(message: Student, reader: jspb.BinaryReader): Student;
 }
 
 export namespace Student {
-  export type AsObject = {
-    id: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-  };
+    export type AsObject = {
+        id: string,
+        firstName: string,
+        lastName: string,
+        email: string,
+    }
 }
 
-export class GetStudentsRequest extends jspb.Message {
-  serializeBinary(): Uint8Array;
-  toObject(includeInstance?: boolean): GetStudentsRequest.AsObject;
-  static toObject(
-    includeInstance: boolean,
-    msg: GetStudentsRequest
-  ): GetStudentsRequest.AsObject;
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
-  static extensionsBinary: {
-    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
-  };
-  static serializeBinaryToWriter(
-    message: GetStudentsRequest,
-    writer: jspb.BinaryWriter
-  ): void;
-  static deserializeBinary(bytes: Uint8Array): GetStudentsRequest;
-  static deserializeBinaryFromReader(
-    message: GetStudentsRequest,
-    reader: jspb.BinaryReader
-  ): GetStudentsRequest;
+export class GetStudentsRequest extends jspb.Message { 
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): GetStudentsRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: GetStudentsRequest): GetStudentsRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: GetStudentsRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): GetStudentsRequest;
+    static deserializeBinaryFromReader(message: GetStudentsRequest, reader: jspb.BinaryReader): GetStudentsRequest;
 }
 
 export namespace GetStudentsRequest {
-  export type AsObject = {};
+    export type AsObject = {
+    }
 }
 
-export class GetStudentsResponse extends jspb.Message {
-  clearStudentList(): void;
-  getStudentList(): Array<Student>;
-  setStudentList(value: Array<Student>): GetStudentsResponse;
-  addStudent(value?: Student, index?: number): Student;
+export class GetStudentsResponse extends jspb.Message { 
+    clearStudentList(): void;
+    getStudentList(): Array<Student>;
+    setStudentList(value: Array<Student>): GetStudentsResponse;
+    addStudent(value?: Student, index?: number): Student;
 
-  serializeBinary(): Uint8Array;
-  toObject(includeInstance?: boolean): GetStudentsResponse.AsObject;
-  static toObject(
-    includeInstance: boolean,
-    msg: GetStudentsResponse
-  ): GetStudentsResponse.AsObject;
-  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
-  static extensionsBinary: {
-    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
-  };
-  static serializeBinaryToWriter(
-    message: GetStudentsResponse,
-    writer: jspb.BinaryWriter
-  ): void;
-  static deserializeBinary(bytes: Uint8Array): GetStudentsResponse;
-  static deserializeBinaryFromReader(
-    message: GetStudentsResponse,
-    reader: jspb.BinaryReader
-  ): GetStudentsResponse;
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): GetStudentsResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: GetStudentsResponse): GetStudentsResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: GetStudentsResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): GetStudentsResponse;
+    static deserializeBinaryFromReader(message: GetStudentsResponse, reader: jspb.BinaryReader): GetStudentsResponse;
 }
 
 export namespace GetStudentsResponse {
-  export type AsObject = {
-    studentList: Array<Student.AsObject>;
-  };
+    export type AsObject = {
+        studentList: Array<Student.AsObject>,
+    }
 }
