51dec14bd80697fdd79876bac69e70d18130a9c1
amantri <anup.mantri@gmail.com>
2023-03-23 11:14:40 -0700

    Working Google oauth

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,10 +1,19 @@
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { trpc } from "@/lib/trpc";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { useState } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+interface CustomPageProps {
+  session: Session;
+}
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<CustomPageProps>) {
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
@@ -19,7 +28,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <SessionProvider session={pageProps.session}>
+          <Component {...pageProps} />
+        </SessionProvider>
       </QueryClientProvider>
     </trpc.Provider>
   );
