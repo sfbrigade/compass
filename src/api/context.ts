2c81fd355502361f349ac179b5da425c2c78f5e3
Max Isom <hi@maxisom.me>
2023-03-29 20:28:46 -0700

    Lint fixes

--- src/api/context.ts
+++ src/api/context.ts
@@ -3,7 +3,7 @@ import { getDb } from "api/db/lib/get-db";
 import { Env } from "./lib/types";
 
 export const createContext = (options: CreateNextContextOptions) => {
-  const { DATABASE_URL } = options.req.env as unknown as Env ?? process.env;
+  const { DATABASE_URL } = (options.req.env as unknown as Env) ?? process.env;
   return {
     ...getDb(DATABASE_URL),
   };
