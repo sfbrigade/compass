f71bf9df5b110d8514834d813453970ea2d62dff
Max Isom <hi@maxisom.me>
2023-04-09 21:45:34 -0700

    Add custom auth adapter

--- src/backend/db/migrations/1_initial-migrations.sql
+++ src/backend/db/migrations/1_initial-migrations.sql
@@ -4,7 +4,32 @@ CREATE TABLE "user" (
   user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
-  role TEXT NOT NULL CHECK (role IN ('para', 'case_manager', 'admin'))
+  role TEXT NOT NULL CHECK (role IN ('para', 'case_manager', 'admin')),
+  email TEXT UNIQUE NOT NULL,
+  email_verified_at TIMESTAMPTZ,
+  image_url TEXT
+);
+
+CREATE TABLE "account" (
+  account_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_id UUID REFERENCES "user" (user_id) ON DELETE CASCADE,
+  provider_name TEXT NOT NULL,
+  provider_account_id TEXT NOT NULL,
+  access_token TEXT,
+  refresh_token TEXT,
+  expires_at INT,
+  token_type TEXT,
+  scope TEXT,
+  id_token TEXT,
+  session_state TEXT,
+  UNIQUE (provider_name, provider_account_id)
+);
+
+CREATE TABLE "session" (
+  session_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  session_token TEXT NOT NULL UNIQUE,
+  user_id UUID NOT NULL REFERENCES "user" (user_id) ON DELETE CASCADE,
+  expires_at TIMESTAMPTZ NOT NULL
 );
 
 CREATE TABLE "student" (
