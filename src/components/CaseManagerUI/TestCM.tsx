cce95e9ae77dabb795f2c248bc2f07c725db01e4
test <test@macbook-pro-2.attlocal.net>
2023-04-10 18:25:02 -0700

    add dashboard files to pages and create components folder

---
+++ src/components/CaseManagerUI/TestCM.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestComponent = () => {
+  return (
+    <div>
+      <h1>Student Goes Here</h1>
+    </div>
+  );
+};
+
+export default TestComponent;
---
+++ src/pages/cmDashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TestComponent from "../components/CaseManagerUI/TestCM";
+
+function cmDashboard() {
+  return (
+    <div>
+      <p>This is the CM Dashboard</p>
+      <TestComponent />
+    </div>
+  );
+}
+
+export default cmDashboard;
