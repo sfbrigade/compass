9274d99cbb4184c9cb90590d06b389d4ab8f687b
Kelly <kellyshao728@gmail.com>
2023-04-14 23:32:29 -0700

    Removed Homepage Default CSS + Path Change for Imgs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import logo from "../styles/img/compass-logo.png";
+import logo from "../public/img/compass-logo.png";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage = () => {
