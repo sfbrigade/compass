88a36eaa53ca8f8ddd43cc391604c4d78a97207b
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-05-30 23:13:31 -0700

    Remove comment

--- src/components/case_manager/AllParas.tsx
+++ src/components/case_manager/AllParas.tsx
@@ -21,7 +21,6 @@ const AllParasPage = () => {
       email: data.get("email") as string,
       role: "para",
     });
-    // email invitation here? Or should be in the backend as part of mutate?
   };
 
   if (isLoading) {
