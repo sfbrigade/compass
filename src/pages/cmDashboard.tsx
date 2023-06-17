9cfb4a04743fe6cfefd1e1adfbdc7e155f1a0d45
jonahchoi <cjonah227@gmail.com>
2023-06-17 13:34:50 -0700

    Moved head(title, favicon, etc.) to _app, for consistency across app.

--- src/pages/cmDashboard.tsx
+++ src/pages/cmDashboard.tsx
@@ -2,17 +2,11 @@ import React from "react";
 import MyParas from "../components/case_manager/MyParas";
 import MyStudentsPage from "../components/case_manager/MyStudents";
 import styles from "../styles/Dashboard.module.css";
-import Head from "next/head";
 import Image from "next/image";
 
 function cmDashboard() {
   return (
     <div className={styles.cmContainer}>
-      <Head>
-        <title>Compass</title>
-        <meta name="description" content="Make IEPs easier" />
-        <link rel="icon" href="/img/favicon.png" />
-      </Head>
       <nav style={{ padding: "2%" }}>
         <Image
           src="/img/compass-logo.svg"
