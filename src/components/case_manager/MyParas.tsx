b89bb14e1fd9c41c94d96187a9b5134618e55e44
Hieu Ngo <hieu.ngo12989@gmail.com>
2023-06-21 20:10:43 -0700

    Copied AllPara.tsx to MyPara.tsx

---
+++ src/components/case_manager/MyParas.tsx
@@ -0,0 +1,51 @@
+import { trpc } from "client/lib/trpc";
+import React from "react";
+import styles from "../../styles/Dashboard.module.css";
+import Link from "next/link";
+import PersonCreationForm from "./PersonCreationForm";
+
+const MyParasPage = () => {
+  const utils = trpc.useContext();
+  const { data: paras, isLoading } = trpc.para.getMyParas.useQuery();
+
+  const assignParaToCaseManager = trpc.para.assignParaToCaseManager.useMutation(
+    {
+      onSuccess: () => utils.para.getMyParas.invalidate(),
+      onError: (error) => alert(error.message),
+    }
+  );
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    assignParaToCaseManager.mutate({
+      first_name: data.get("first_name") as string,
+      last_name: data.get("last_name") as string,
+      email: data.get("email") as string,
+    });
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <PersonCreationForm title={"Add a Para"} onSubmit={handleSubmit} />
+
+      <h2>All Paras</h2>
+      <ul className={styles.listNames}>
+        {paras?.map((para) => (
+          <li key={para.user_id}>
+            <Link href={`/paras/${para.user_id}`}>
+              {para.first_name} {para.last_name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MyParasPage;
