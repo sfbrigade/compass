bb85762036d98dacc47bbec81673054641d882a7
Brandon Cruz-Youll <42663258+BeeSeeWhy@users.noreply.github.com>
2023-06-26 22:38:44 -0700

    Update src/pages/_app.tsx
    Co-authored-by: Max Isom <codetheweb@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -32,7 +32,6 @@ export default function App({
   Component,
   pageProps,
 }: AppProps<CustomPageProps>) {
-  const emotionCache = clientSideEmotionCache;
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
