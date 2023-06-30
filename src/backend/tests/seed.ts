00f2137928af1e3ffaebeab09b6e9567e53c291d
amantri <anup.mantri@gmail.com>
2023-06-29 23:50:36 -0700

    Add a case_manager and a student to the seed data, and remove para2.
    Use the case_manager for tests where it makes sense.

--- src/backend/tests/seed.ts
+++ src/backend/tests/seed.ts
@@ -3,43 +3,53 @@ import { KyselyDatabaseInstance } from "@/backend/lib";
 export type SeedResult = Awaited<ReturnType<typeof seed>>;
 
 export const seed = async (db: KyselyDatabaseInstance) => {
-  const para = await db
-    .insertInto("user")
-    .values({
-      first_name: "Emily",
-      last_name: "Smith",
-      email: "emily@example.com",
-      role: "staff",
-    })
-    .returningAll()
-    .executeTakeFirstOrThrow();
-
   const admin = await db
     .insertInto("user")
     .values({
-      first_name: "John",
-      last_name: "Doe",
-      email: "john@example.com",
+      first_name: "Admin",
+      last_name: "One",
+      email: "admin1@example.com",
       role: "admin",
     })
     .returningAll()
     .executeTakeFirstOrThrow();
 
-  //* reusable para seed for testing
-  const para2 = await db
+  const case_manager = await db
     .insertInto("user")
     .values({
-      first_name: "Alexander",
-      last_name: "Maximus",
-      email: "alexander@example.com",
+      first_name: "CaseManager",
+      last_name: "One",
+      email: "case_manager1@example.com",
       role: "staff",
     })
     .returningAll()
     .executeTakeFirstOrThrow();
 
+  const para = await db
+    .insertInto("user")
+    .values({
+      first_name: "Para",
+      last_name: "One",
+      email: "para1@example.com",
+      role: "staff",
+    })
+    .returningAll()
+    .executeTakeFirstOrThrow();
+
+  const student = await db
+    .insertInto("student")
+    .values({
+      first_name: "Student",
+      last_name: "One",
+      email: "student1@example.com",
+    })
+    .returningAll()
+    .executeTakeFirstOrThrow();
+
   return {
-    para,
     admin,
-    para2,
+    case_manager,
+    student,
+    para,
   };
 };
