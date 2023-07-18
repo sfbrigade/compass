448948c8cbae3b864c4554c61480a8b5fc33aa09
Payam Katoozian <einpanzerattacken@gmail.com>
2023-07-17 18:59:25 -0700

    Created sidebar, links hidden when user is signed out > > Co-authored by: Hannah McGowan <hannahmcg@users.noreply.github.com

---
+++ src/components/case_manager/NavBar.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styles from "../../styles/Navbar.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { BsPersonWorkspace, BsPeople, BsGear } from "react-icons/bs";
+import { FiLogOut } from "react-icons/fi";
+import { signOut } from "next-auth/react";
+import { trpc } from "@/client/lib/trpc";
+
+const NavBar = ({ children }) => {
+  const { data: me } = trpc.user.getMe.useQuery();
+
+  return (
+    <React.Fragment>
+      <div className={styles.container}>
+        <div className={styles.sidebar}>
+          <Link href="">
+            <Image
+              src="/img/compass-logo.svg"
+              alt="logo"
+              className={styles.logo}
+              width={64}
+              height={64}
+              priority
+            />
+          </Link>
+          <br />
+
+          {me && (
+            <div className={styles.linkContainer}>
+              <Link href="" className={styles.link}>
+                <BsPersonWorkspace className={styles.icon} />
+                <p className={styles.linkTitle}>Staff</p>
+              </Link>
+              <br />
+              <Link href="" className={styles.link}>
+                <BsPeople className={styles.icon} />
+                <p className={styles.linkTitle}>Students</p>
+              </Link>
+              <br />
+              <Link href="" className={styles.link}>
+                <BsGear className={styles.icon} />
+                <p className={styles.linkTitle}>Settings</p>
+              </Link>
+              <br />
+              <Link href="" className={styles.link}>
+                <FiLogOut className={styles.icon} />
+                <p
+                  className={styles.linkTitle}
+                  onClick={() =>
+                    signOut({ callbackUrl: "http://localhost:3000/" })
+                  }
+                >
+                  Logout
+                </p>
+              </Link>
+            </div>
+          )}
+        </div>
+        <div className={styles.body}>{children}</div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default NavBar;
