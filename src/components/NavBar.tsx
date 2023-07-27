c28937402b6f1e916eba32e82ad02f6a3c05d324
Payam Katoozian <KotovSyndrome@users.noreply.github.com>
2023-07-27 13:27:32 -0700

    Created sidebar, links hidden when user is signed out (#149)
    * Created sidebar, links hidden when user is signed out
    >
    >
    Co-authored by: Hannah McGowan <hannahmcg@users.noreply.github.com
    
    * added links to student and para tables, settings page under construction
    
    * redirect to students page upon login
    
    * updated table title
    
    * ran prettier on navbar styles
    
    * added type to Navbar props
    
    * resolved all issues except 401 error, fixed linting
    
    * removed React.FC, updated prop type
    
    * Pascal Casing
    
    * updated component exports
    
    * Fixed export casing

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  PeopleOutline,
+  CoPresent,
+  Settings,
+  Logout,
+} from "@mui/icons-material";
+import { signOut } from "next-auth/react";
+import { trpc } from "@/client/lib/trpc";
+
+interface NavBarProps {
+  children: React.ReactNode;
+}
+
+const NavBar = ({ children }: NavBarProps) => {
+  const { data: me } = trpc.user.getMe.useQuery();
+
+  return (
+    <React.Fragment>
+      <div className={styles.container}>
+        <div className={styles.sidebar}>
+          <Link href="/">
+            <Image
+              src="/img/compass-logo.svg"
+              alt="logo"
+              className={styles.logo}
+              width={64}
+              height={64}
+              priority
+            />
+          </Link>
+          <br />
+
+          {me && (
+            <div className={styles.linkContainer}>
+              <Link href="/students" className={styles.link}>
+                <PeopleOutline className={styles.icon} />
+                <p className={styles.linkTitle}>Students</p>
+              </Link>
+              <br />
+              <Link href="/staff" className={styles.link}>
+                <CoPresent className={styles.icon} />
+                <p className={styles.linkTitle}>Staff</p>
+              </Link>
+              <br />
+              <Link href="/settings" className={styles.link}>
+                <Settings className={styles.icon} />
+                <p className={styles.linkTitle}>Settings</p>
+              </Link>
+              <br />
+              <Link href="" className={styles.link}>
+                <Logout className={styles.icon} />
+                <p
+                  className={styles.linkTitle}
+                  onClick={() => signOut({ callbackUrl: "/" })}
+                >
+                  Logout
+                </p>
+              </Link>
+            </div>
+          )}
+        </div>
+        <div className={styles.body}>{children}</div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/staff/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "../../styles/Dashboard.module.css";
+import MyParas from "@/components/case_manager/MyParas";
+
+function Staff() {
+  return (
+    <div className={styles.cmContainer}>
+      <nav style={{ padding: "2%" }}></nav>
+      <main>
+        <div style={{ padding: "20px", textAlign: "center" }}>
+          <div
+            style={{
+              display: "flex",
+              alignContent: "center",
+              justifyContent: "center",
+            }}
+          >
+            <MyParas />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Staff;
