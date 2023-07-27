69217822ccdb564d36123ff1039c107bf9141328
Jonah Choi <104597125+jonahchoi@users.noreply.github.com>
2023-07-27 14:57:41 -0700

    MVP data models for goals, subgoals(benchmarks), tasks. (#157)
    * New task and data tables
    
    * Added task and data tables. Edited paraDash and taskCards to use db
    
    * Updated schemas
    
    * Updated all paratrial schemas. Updated routes and components to reflect changes.
    
    * Added new tests for para routes, adjusted test for iep route
    
    * Adjusted schemas, removed unrelated code.
    
    * Add todos in iep.test

--- src/backend/routers/iep.test.ts
+++ src/backend/routers/iep.test.ts
@@ -1,8 +1,9 @@
 import test from "ava";
 import { getTestServer } from "@/backend/tests";
 
-test("basic flow - add/get goals and subgoals", async (t) => {
-  const { trpc, seed } = await getTestServer(t, {
+// TODO: Write more tests
+test("basic flow - add/get goals, subgoals, tasks", async (t) => {
+  const { trpc, db, seed } = await getTestServer(t, {
     authenticateAs: "case_manager",
   });
 
@@ -15,14 +16,24 @@ test("basic flow - add/get goals and subgoals", async (t) => {
   const goal1 = await trpc.iep.addGoal.mutate({
     iep_id: iep.iep_id,
     description: "goal 1",
+    category: "writing",
   });
   await trpc.iep.addSubgoal.mutate({
     goal_id: goal1!.goal_id,
     description: "subgoal 1",
+    instructions: "instructions here",
+    target_max_attempts: 5,
   });
-  await trpc.iep.addSubgoal.mutate({
+  const subgoal2 = await trpc.iep.addSubgoal.mutate({
     goal_id: goal1!.goal_id,
     description: "subgoal 2",
+    instructions: "",
+    target_max_attempts: null,
+  });
+  await trpc.iep.addTask.mutate({
+    subgoal_id: subgoal2!.subgoal_id,
+    assignee_id: seed.para.user_id,
+    due_date: new Date("2023-12-31"),
   });
 
   const gotGoals = await trpc.iep.getGoals.query({ iep_id: iep.iep_id });
@@ -32,4 +43,14 @@ test("basic flow - add/get goals and subgoals", async (t) => {
     goal_id: goal1!.goal_id,
   });
   t.is(gotSubgoals.length, 2);
+
+  // TODO: Don't query db directly and use an API method instead. Possibly create a getTasks method later
+  t.truthy(
+    await db
+      .selectFrom("task")
+      .where("subgoal_id", "=", subgoal2!.subgoal_id)
+      .where("assignee_id", "=", seed.para.user_id)
+      .selectAll()
+      .executeTakeFirstOrThrow()
+  );
 });
--- src/components/Subgoal.tsx
+++ src/components/Subgoal.tsx
@@ -1,20 +1,41 @@
+import { trpc } from "@/client/lib/trpc";
 import React from "react";
+import { Subgoal } from "@/types/global";
 
 interface SubgoalProps {
   subgoal: Subgoal;
 }
 
-interface Subgoal {
-  subgoal_id: string;
-  description: string | null;
-}
-
 const Subgoals = ({ subgoal }: SubgoalProps) => {
+  const task = trpc.iep.addTask.useMutation();
+  // TODO: add form to assign to my paras
+  // const assignToPara = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+
+  //   task.mutate({
+  //     subgoal_id: subgoal.subgoal_id,
+  //     assignee_id: data.get("assignee_id") as string,
+  //     due_date: new Date(data.get("due_date")) as Date
+  //   });
+  // }
+  const assignToPara = () => {
+    task.mutate({
+      subgoal_id: subgoal.subgoal_id,
+      assignee_id: "51c24050-103e-4183-b9ad-6ce6bea0e062",
+      due_date: new Date(),
+    });
+  };
+
   return (
     <div>
       <h4>Subgoal</h4>
       <div>Subgoal ID: {subgoal.subgoal_id}</div>
-      <p>{subgoal.description}</p>
+      <p>Description: {subgoal.description}</p>
+      <p>Created at: {subgoal.created_at.toDateString()}</p>
+      <p>Instructions: {subgoal.instructions || "null"}</p>
+      <p>Target max attempts: {subgoal.target_max_attempts || "null"}</p>
+      <button onClick={assignToPara}>Assign</button>
     </div>
   );
 };
