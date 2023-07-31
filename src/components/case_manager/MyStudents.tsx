10fdec06921c6b9da927588812720a7ebf17c84c
Tessa Thornberry <tthornberryclass@gmail.com>
2023-07-31 15:02:14 -0700

    filled out comment for form reset

--- src/components/case_manager/MyStudents.tsx
+++ src/components/case_manager/MyStudents.tsx
@@ -26,7 +26,7 @@ const MyStudents = () => {
       last_name: data.get("last_name") as string,
       email: data.get("email") as string,
     });
-    // if form remains visible upon adding a student, this clears the form, however, due to Material UI limitations, it remains "filled"
+    // resetting the form this way is only necessary if the form remains visible upon adding a person. due to Materials UI, the reset form(s) will show as "touched" (TT).
     (event.target as HTMLFormElement).reset();
   };
 
