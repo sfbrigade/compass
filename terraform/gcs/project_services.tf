b3aedab6f981ae467abe38a961bc9f4c0b3941f5
Max Isom <hi@maxisom.me>
2023-08-01 20:32:41 -0700

    Revert "Enable cloud resource management API"
    This reverts commit fa462ddaf041e93172a652a1f0f770dcecc7b2fc.

--- terraform/gcs/project_services.tf
+++ terraform/gcs/project_services.tf
@@ -12,18 +12,3 @@ resource "google_project_service" "sqladmin_api" {
 resource "google_project_service" "cloud_run_service" {
   service = "run.googleapis.com"
 }
-
-# Enable Cloud Resource Manager API (needed when deploying from CI but not when deploying locally...)
-resource "google_project_service" "cloud_resource_manager_api" {
-  project = var.project_id
-  service = "cloudresourcemanager.googleapis.com"
-}
-
-# Wait for Cloud Resource Manager API to be enabled
-resource "time_sleep" "gcp_wait_crm_api_enabling" {
-  depends_on = [
-    google_project_service.cloud_resource_manager_api
-  ]
-
-  create_duration = "1m"
-}
