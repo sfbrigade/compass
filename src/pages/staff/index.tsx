59de901be094c224d30abd30cd35d8746d2824b4
Jonah Choi <104597125+jonahchoi@users.noreply.github.com>
2023-08-07 00:21:49 -0700

    CSS Restructuring (#169)
    * added design system variables to globals css and edited module files accordingly
    
    * Added css variables and generic styling. Moved some modules
    
    * Added global modules, modifed components to use.
    
    * Recommit
    
    * Added css module typing.
    
    * made most of colors use root variables
    
    * Added typecheck to PRs, new all command.
    
    * Update typography styles in globals.css
    
    * Removed invalid name from pr.yml
    
    * Fixed lint issues
    
    * Added type-css to postinstall, removed d.ts files from git.
    
    * fixed lint error
    
    * renamed action back
    
    ---------
    
    Co-authored-by: test <test@macbook-pro-2.attlocal.net>
    Co-authored-by: Patrick Choi <ptrckchoi@gmail.com>

--- src/pages/staff/index.tsx
+++ src/pages/staff/index.tsx
@@ -1,26 +1,8 @@
 import React from "react";
-import styles from "../../styles/Dashboard.module.css";
 import MyParas from "@/components/case_manager/MyParas";
 
 function Staff() {
-  return (
-    <div className={styles.cmContainer}>
-      <nav style={{ padding: "2%" }}></nav>
-      <main>
-        <div style={{ padding: "20px", textAlign: "center" }}>
-          <div
-            style={{
-              display: "flex",
-              alignContent: "center",
-              justifyContent: "center",
-            }}
-          >
-            <MyParas />
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+  return <MyParas />;
 }
 
 export default Staff;
