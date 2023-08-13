f42fecb5cc61397df18484bf96c1eddb95fe57a2
jonahchoi <cjonah227@gmail.com>
2023-08-13 14:51:48 -0700

    Modified benchmark collection to match FIGMA MVP. Added para nav buttons

--- src/pages/benchmarks/[benchmark_id]/index.tsx
+++ src/pages/benchmarks/[benchmark_id]/index.tsx
@@ -1,24 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Counter from "@/components/counter/counter";
-import Timer from "@/components/timer/timer";
-import TimerInput from "@/components/timer/timerInputPad";
+import $box from "@/styles/Box.module.css";
 import $button from "@/styles/Button.module.css";
 import { trpc } from "@/client/lib/trpc";
 import { useRouter } from "next/router";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ParaNav from "@/components/ParaNav";
 
 const BenchmarkPage = () => {
   const router = useRouter();
   const { benchmark_id } = router.query;
   const utils = trpc.useContext();
-  const { data: task, isLoading } = trpc.iep.getTaskById.useQuery({
-    task_id: benchmark_id as string,
-  });
+  const {
+    data: task,
+    isLoading,
+    isError,
+  } = trpc.iep.getTaskById.useQuery(
+    {
+      task_id: benchmark_id as string,
+    },
+    {
+      enabled: Boolean(benchmark_id),
+    }
+  );
   const seenMutation = trpc.iep.setSeen.useMutation({
     onSuccess: () => utils.iep.getTaskById.invalidate(),
   });
 
-  const [timerTimeInSec, setTimerTimeInSec] = useState(0);
-  const [timerInputIsOn, setTimerInputIsOn] = useState(false);
+  // const [timerTimeInSec, setTimerTimeInSec] = useState(0);
+  // const [timerInputIsOn, setTimerInputIsOn] = useState(false);
+  // const [runningTotal, setRunningTotal] = useState(0);
 
   useEffect(() => {
     if (task && !task.seen) {
@@ -26,58 +38,63 @@ const BenchmarkPage = () => {
     }
   }, [task, seenMutation]);
 
-  const handleStartTimer = (inputTimeInSec: number) => {
-    setTimerTimeInSec(inputTimeInSec);
-    setTimerInputIsOn(false);
-  };
+  // Timers removed from current MVP. May reintroduce them later.
+  // const handleStartTimer = (inputTimeInSec: number) => {
+  //   setTimerTimeInSec(inputTimeInSec);
+  //   setTimerInputIsOn(false);
+  // };
 
-  const handleSetTimer = () => {
-    setTimerInputIsOn(!timerInputIsOn);
-  };
+  // const handleSetTimer = () => {
+  //   setTimerInputIsOn(!timerInputIsOn);
+  // };
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
+  if (isError) {
+    return <div>Oops! Something went wrong.</div>;
+  }
+
   return (
-    <div>
-      <h1>Trial</h1>
-      <div>
-        <p>
-          <strong>Task:</strong> Given sentence starters and spelling support
-          Annie will write 5 sentences of their own thoughts and opinions with
-          80% accuracy in 4 out of 5 trials.
-        </p>
-      </div>
-      <div>
-        <button
-          className={`${$button.default} ${$button.reversed} ${$button.pilled}`}
-          onClick={handleSetTimer}
-        >
-          Set timer
+    <>
+      <ParaNav />
+      <p className={$box.default}>
+        <strong>Task:</strong> {task.description}
+      </p>
+      <div className={`${$box.topAndBottomBorder} ${$box.flex}`}>
+        <button className={`${$button.default} ${$button.circular}`}>
+          <ChevronLeftIcon />
+        </button>
+        <p>Trial #</p>
+        <button className={`${$button.default} ${$button.circular}`}>
+          <ChevronRightIcon />
         </button>
       </div>
-      {timerInputIsOn ? (
-        <TimerInput onStartTimer={handleStartTimer} />
-      ) : (
-        <div>
-          <Timer timeInSec={timerTimeInSec} />
-          <Counter title="Attempts" maxCount={5} minCount={0} color="blue" />
-          <Counter
-            title="Successful without prompt"
-            maxCount={5}
-            minCount={0}
-            color="green"
-          />
-          <Counter
-            title="Successful with prompt"
-            maxCount={5}
-            minCount={0}
-            color="yellow"
-          />
-        </div>
-      )}
-    </div>
+
+      <div className={`${$box.default} ${$box.greyBg}`}>
+        <Counter
+          title="Successful without prompt"
+          maxCount={5}
+          minCount={0}
+          color="green"
+        />
+        <Counter
+          title="Successful with prompt"
+          maxCount={5}
+          minCount={0}
+          color="yellow"
+        />
+        <Counter title="Attempts" maxCount={5} minCount={0} color="blue" />
+      </div>
+      <input
+        className={$box.default}
+        type="text"
+        placeholder="Type your observation notes here..."
+      ></input>
+
+      <button className={`${$button.default} ${$box.fullWidth}`}>Review</button>
+    </>
   );
 };
 
---
+++ src/pages/benchmarks/[benchmark_id]/instructions.tsx
@@ -0,0 +1,13 @@
+import ParaNav from "@/components/ParaNav";
+import React from "react";
+
+const InstructionsPage = () => {
+  return (
+    <div>
+      <ParaNav />
+      put instructions page here
+    </div>
+  );
+};
+
+export default InstructionsPage;
