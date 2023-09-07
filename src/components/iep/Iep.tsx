3bd5c847ba5ba6a7070d8b1273a6808b2f35c5da
Hieu Ngo <hieu.ngo12989@gmail.com>
2023-09-07 15:33:43 -0700

    extracted imports from mui

--- src/components/iep/Iep.tsx
+++ src/components/iep/Iep.tsx
@@ -2,7 +2,9 @@ import { trpc } from "@/client/lib/trpc";
 import Goals from "@/components/goal/Goal";
 import $button from "@/styles/Button.module.css";
 import $input from "@/styles/Input.module.css";
-import { Box, Container, List } from "@mui/material";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import List from "@mui/material/List";
 import Image from "next/image";
 import noGoals from "../../public/img/no-goals-icon.png";
 
--- src/components/subgoal/Subgoal.tsx
+++ src/components/subgoal/Subgoal.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { Subgoal } from "@/types/global";
-import { Box, Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 interface SubgoalProps {
   subgoal: Subgoal;
