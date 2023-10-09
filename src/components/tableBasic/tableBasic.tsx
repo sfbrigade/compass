c4de851f8d641695a26f625bbc89e00b5d56c561
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-10-09 16:37:26 -0700

    Start to create view state for Edit view for individual student. Add VIEW_STATES object and conditionally render based on view state. Add cancel button. Add temporary "tableBasic" as placeholder for EDIT view. Move Archive button to EDIT view. Add basic CSS using MaterialUI. Put in rough draft example of possible editStudent authenticated procedure in routers/case_manager.

---
+++ src/components/tableBasic/tableBasic.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(number: number, item: string, qty: number, price: number) {
+  return { number, item, qty, price };
+}
+
+const rows = [
+  createData(1, "Apple", 5, 3),
+  createData(2, "Orange", 2, 2),
+  createData(3, "Grapes", 3, 1),
+  createData(4, "Tomato", 2, 1.6),
+  createData(5, "Mango", 1.5, 4),
+];
+
+export default function BasicTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>S.No</TableCell>
+            <TableCell align="right">Item</TableCell>
+            <TableCell align="right">Quantity&nbsp;(kg)</TableCell>
+            <TableCell align="right">Price&nbsp;($)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.number}>
+              <TableCell component="th" scope="row">
+                {row.number}
+              </TableCell>
+              <TableCell align="right">{row.item}</TableCell>
+              <TableCell align="right">{row.qty}</TableCell>
+              <TableCell align="right">{row.price}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
