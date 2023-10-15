0dd6411307ddff9916c3038cd543a3f77ffb7ff8
SeldomseenSchweig <jamesschweig@gmail.com>
2023-10-14 21:39:21 -0700

    created home button

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,21 @@
 import React, { useEffect } from "react";
+import $button from "@/styles/Button.module.css";
+import { useRouter } from "next/router";
 
 const About = () => {
-  return <div>About Page</div>;
+  const router = useRouter();
+
+  return (
+    <>
+      <button
+        className={$button.about}
+        onClick={() => router.push("/signInPage")}
+      >
+        home
+      </button>
+      <div>About Page</div>
+    </>
+  );
 };
 
 export default About;
