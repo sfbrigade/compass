a6264c5d0b79531d08bdd4e7a5d9e485b98cc745
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-10-19 12:20:04 -0700

    Replace TableCell with Input in EditStudentTable. Import student object to populate rows with current student info. Add StudentType and EditStudentTableProps. Add display gridTemplateColumns to TableRows. Add student prop to student_id.tsx.

--- src/components/editStudentTable/EditStudentTable.tsx
+++ src/components/editStudentTable/EditStudentTable.tsx
@@ -4,35 +4,49 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableRow from "@mui/material/TableRow";
+import Input from "@mui/material/Input";
 import Box from "@mui/material/Box";
 
-const data = [
-  { label: "First Name", value: "David" },
-  { label: "Last Name", value: "Belinda" },
-  { label: "Date of Birth", value: "08/18/2010" },
-  { label: "Grade", value: "5" },
-  { label: "Email Address", value: "abc@gmail.com" },
-];
+interface StudentType {
+  first_name: string;
+  last_name: string;
+  email: string;
+  grade: number;
+}
 
-export default function EditStudentTable() {
+interface EditStudentTableProps {
+  student: StudentType;
+}
+
+export default function EditStudentTable({ student }: EditStudentTableProps) {
+  const { first_name, last_name, email, grade } = student;
+
+  const data = [
+    { label: "First Name", value: first_name },
+    { label: "Last Name", value: last_name },
+    { label: "Date of Birth", value: "08/18/2010" },
+    { label: "Grade", value: grade },
+    { label: "Email Address", value: email },
+  ];
+
+  // console.log("student in EditStudentTable: ", student);
   return (
     <TableContainer>
-      <Table aria-label="simple table">
+      <Table aria-label="Edit student table">
         <TableBody>
           {data.map((row) => (
             <TableRow
+              sx={{
+                display: "grid",
+                gridTemplateColumns: "300px 100px 300px 1fr",
+              }}
               key={row.label}
-              // sx={{
-              //   padding: "8px"
-              // }}
             >
               <TableCell padding="none" sx={{ padding: "1rem" }}>
                 {row.label}
               </TableCell>
               <Box sx={{ width: "2px", padding: "1rem" }}>:</Box>
-              <TableCell padding="none" sx={{ padding: "1rem" }}>
-                {row.value}
-              </TableCell>
+              <Input placeholder={row.value.toString()}></Input>
             </TableRow>
           ))}
         </TableBody>
