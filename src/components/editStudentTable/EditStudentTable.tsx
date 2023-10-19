1f29e036dfd3128f1328d75d08deb0a43489d164
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-10-19 15:20:27 -0700

    Refactor EditStudentTable - remove row logic and make into separate component EditStudentRow. Create state for each input item in student_id.tsx. Create types for all props in EditStudentTable and EditStudentRow.

--- src/components/editStudentTable/EditStudentTable.tsx
+++ src/components/editStudentTable/EditStudentTable.tsx
@@ -1,54 +1,54 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
-import TableRow from "@mui/material/TableRow";
-import Input from "@mui/material/Input";
-import Box from "@mui/material/Box";
-
-interface StudentType {
-  first_name: string;
-  last_name: string;
-  email: string;
-  grade: number;
-}
+import EditStudentRow from "../editStudentRow/EditStudentRow";
 
 interface EditStudentTableProps {
-  student: StudentType;
+  firstName: string;
+  setFirstName: Dispatch<SetStateAction<string>>;
+  lastName: string;
+  setLastName: Dispatch<SetStateAction<string>>;
+  email: string;
+  setEmail: Dispatch<SetStateAction<string>>;
+  grade: string | number;
+  setGrade: Dispatch<SetStateAction<string | number>>;
 }
 
-export default function EditStudentTable({ student }: EditStudentTableProps) {
-  const { first_name, last_name, email, grade } = student;
-
-  const data = [
-    { label: "First Name", value: first_name },
-    { label: "Last Name", value: last_name },
-    { label: "Date of Birth", value: "08/18/2010" },
-    { label: "Grade", value: grade },
-    { label: "Email Address", value: email },
-  ];
-
-  // console.log("student in EditStudentTable: ", student);
+export default function EditStudentTable({
+  firstName,
+  setFirstName,
+  lastName,
+  setLastName,
+  email,
+  setEmail,
+  grade,
+  setGrade,
+}: EditStudentTableProps) {
   return (
     <TableContainer>
       <Table aria-label="Edit student table">
         <TableBody>
-          {data.map((row) => (
-            <TableRow
-              sx={{
-                display: "grid",
-                gridTemplateColumns: "300px 100px 300px 1fr",
-              }}
-              key={row.label}
-            >
-              <TableCell padding="none" sx={{ padding: "1rem" }}>
-                {row.label}
-              </TableCell>
-              <Box sx={{ width: "2px", padding: "1rem" }}>:</Box>
-              <Input placeholder={row.value.toString()}></Input>
-            </TableRow>
-          ))}
+          <EditStudentRow
+            label="First Name"
+            value={firstName}
+            handleInputChange={(e) => setFirstName(e.target.value)}
+          />
+          <EditStudentRow
+            label="Last Name"
+            value={lastName}
+            handleInputChange={(e) => setLastName(e.target.value)}
+          />
+          <EditStudentRow
+            label="Grade"
+            value={grade}
+            handleInputChange={(e) => setGrade(parseInt(e.target.value))}
+          />
+          <EditStudentRow
+            label="Email Address"
+            value={email}
+            handleInputChange={(e) => setEmail(e.target.value)}
+          />
         </TableBody>
       </Table>
     </TableContainer>
