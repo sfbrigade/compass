6adc8ebbd7c06b584a1a61a5998ee1b97d944f42
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-10-20 10:49:57 -0700

    Change Input to Textfield in EditStudentRow. Remove Box due to div error in Table. Wrap Textfield in TableCell and customize attributes.

--- src/components/editStudentRow/EditStudentRow.tsx
+++ src/components/editStudentRow/EditStudentRow.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
-<<<<<<< HEAD
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-=======
-import Input from "@mui/material/Input";
-import Box from "@mui/material/Box";
->>>>>>> 1f29e03 (Refactor EditStudentTable - remove row logic and make into separate component EditStudentRow. Create state for each input item in student_id.tsx. Create types for all props in EditStudentTable and EditStudentRow.)
 
 interface EditStudentRowProps {
   label: string;
@@ -21,7 +16,6 @@ export default function EditStudentRow({
   handleInputChange,
 }: EditStudentRowProps) {
   return (
-<<<<<<< HEAD
     <Table>
       <TableBody>
         <TableRow
@@ -63,23 +57,5 @@ export default function EditStudentRow({
         </TableRow>
       </TableBody>
     </Table>
-=======
-    <TableRow
-      sx={{
-        display: "grid",
-        gridTemplateColumns: "300px 100px 300px 1fr",
-      }}
-      key={label}
-    >
-      <TableCell padding="none" sx={{ padding: "1rem" }}>
-        {label}
-      </TableCell>
-      <Box sx={{ width: "2px", padding: "1rem" }}>:</Box>
-      <Input
-        placeholder={value.toString()}
-        onChange={handleInputChange}
-      ></Input>
-    </TableRow>
->>>>>>> 1f29e03 (Refactor EditStudentTable - remove row logic and make into separate component EditStudentRow. Create state for each input item in student_id.tsx. Create types for all props in EditStudentTable and EditStudentRow.)
   );
 }
