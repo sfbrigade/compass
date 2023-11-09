df6e6499d30f06695e8ce69759232fb6c699efec
Max Isom <codetheweb@users.noreply.github.com>
2023-11-08 19:12:41 -0800

    Add para assignment dialog (#239)

--- src/components/subgoal/Subgoal.tsx
+++ src/components/subgoal/Subgoal.tsx
@@ -2,25 +2,17 @@ import { trpc } from "@/client/lib/trpc";
 import { Subgoal } from "@/types/global";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import { useState } from "react";
+import { SubgoalAssignmentModal } from "./Subgoal-Assignment-Modal";
 
 interface SubgoalProps {
   subgoal: Subgoal;
 }
 
 const Subgoals = ({ subgoal }: SubgoalProps) => {
+  const [isAssignmentModalOpen, setIsAssignmentModalOpen] = useState(false);
   const task = trpc.iep.tempAddTaskToSelf.useMutation();
 
-  // TODO: add form to assign to my paras
-  // const assignToPara = (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  //   const data = new FormData(event.currentTarget);
-
-  //   task.mutate({
-  //     subgoal_id: subgoal.subgoal_id,
-  //     assignee_id: data.get("assignee_id") as string,
-  //     due_date: new Date(data.get("due_date")) as Date
-  //   });
-  // }
   const assignToPara = async () => {
     const result = await task.mutateAsync({
       subgoal_id: subgoal.subgoal_id,
@@ -35,32 +27,55 @@ const Subgoals = ({ subgoal }: SubgoalProps) => {
   };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        backgroundColor: "#f4d5d5",
-        padding: "1rem",
-      }}
-    >
-      <p>{subgoal.description}</p>
-      <Button
+    <>
+      <Box
         sx={{
-          height: "24px",
-          width: "auto",
-          padding: "0px 20px",
-          backgroundColor: "#5347d7",
-          borderRadius: "5px",
-          border: "none",
-          color: "#ffffff",
-          fontFamily: "Quicksand",
+          display: "flex",
+          justifyContent: "space-between",
+          backgroundColor: "#f4d5d5",
+          padding: "1rem",
         }}
-        variant="contained"
-        onClick={assignToPara}
       >
-        Assign
-      </Button>
-    </Box>
+        <p>{subgoal.description}</p>
+        <Button
+          sx={{
+            height: "24px",
+            width: "auto",
+            padding: "0px 20px",
+            backgroundColor: "#5347d7",
+            borderRadius: "5px",
+            border: "none",
+            color: "#ffffff",
+            fontFamily: "Quicksand",
+          }}
+          variant="contained"
+          onClick={assignToPara}
+        >
+          Assign
+        </Button>
+
+        <Button
+          sx={{
+            height: "24px",
+            width: "auto",
+            padding: "0px 20px",
+            backgroundColor: "#5347d7",
+            borderRadius: "5px",
+            border: "none",
+            color: "#ffffff",
+            fontFamily: "Quicksand",
+          }}
+          variant="contained"
+          onClick={() => setIsAssignmentModalOpen(true)}
+        >
+          [Draft] Assign
+        </Button>
+      </Box>
+      <SubgoalAssignmentModal
+        isOpen={isAssignmentModalOpen}
+        onClose={() => setIsAssignmentModalOpen(false)}
+      />
+    </>
   );
 };
 
