6b15b5afdfb46a1543dd03caff276c516b75b00a
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-11-15 17:11:45 -0800

    Staff page - start to build staff page, emulate student page for now. In user_id.tsx, add edit/view states with Save and Cancel buttons, display email address, add basic form with inputs/placeholders, refactor Archive button to match Student archive, add Modal to open when Archive button pressed. Create StaffPage.module.css file.

--- src/pages/staff/[user_id].tsx
+++ src/pages/staff/[user_id].tsx
@@ -1,15 +1,34 @@
 import { useState } from "react";
 import { trpc } from "@/client/lib/trpc";
 import { useRouter } from "next/router";
-import Link from "next/link";
 import $home from "@/styles/Home.module.css";
 import $button from "@/styles/Button.module.css";
+import $StaffPage from "../../styles/StaffPage.module.css";
+import $Modal from "../../styles/Modal.module.css";
+
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Modal from "@mui/material/Modal";
 
 const ViewParaPage = () => {
-  const [unassignParaPrompt, setUnassignParaPrompt] = useState(false);
+  const [archiveParaPrompt, setArchiveParaPrompt] = useState(false);
+  const [viewState, setViewState] = useState(0);
+
   const router = useRouter();
   const { user_id } = router.query;
   const { data: me } = trpc.user.getMe.useQuery();
+
+  const VIEW_STATES = { MAIN: 0, EDIT: 1 };
+
+  const handleEditState = () => {
+    setViewState(VIEW_STATES.EDIT);
+  };
+
+  const handleMainState = () => {
+    setViewState(VIEW_STATES.MAIN);
+  };
 
   const { data: para, isLoading } = trpc.para.getParaById.useQuery(
     { user_id: user_id as string },
@@ -20,7 +39,13 @@ const ViewParaPage = () => {
     }
   );
 
-  const unassignPara = trpc.case_manager.removePara.useMutation({
+  const buttonSX = {
+    "&:hover": {
+      background: "#3023B8",
+    },
+  };
+
+  const archivePara = trpc.case_manager.removePara.useMutation({
     onError: (error) => console.log(error.message),
   });
 
@@ -28,11 +53,11 @@ const ViewParaPage = () => {
     await router.push(`/staff`);
   };
 
-  const handleUnassignPara = async () => {
+  const handleArchivePara = async () => {
     if (!para) return;
 
-    await unassignPara.mutateAsync({ para_id: para.user_id });
-    await returnToStaffList();
+    await archivePara.mutateAsync({ para_id: para.user_id });
+    await router.push(`/staff`);
   };
 
   if (isLoading) {
@@ -42,50 +67,214 @@ const ViewParaPage = () => {
   if (!para) return;
 
   return (
-    <div>
-      <h1>
-        {para.first_name} {para.last_name}
-      </h1>
-      <p>
-        <b>Para ID:</b> {para.user_id}
-      </p>
-      <p>
-        <b>Para Email:</b> {para.email}
-      </p>
+    <Stack
+      spacing={2}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
+      <Container
+        className={$StaffPage.staffInfoContainer}
+        sx={{ marginBottom: "1rem" }}
+      >
+        <Box className={$StaffPage.displayBox}>
+          <p className={$StaffPage.staffName}>
+            {para?.first_name} {para?.last_name}
+          </p>
 
-      {me?.user_id !== user_id && (
-        <button
-          className={`${$button.default} ${$home.bold}`}
-          onClick={() => setUnassignParaPrompt(true)}
-        >
-          Unassign Para
-        </button>
+          {/* Edit button only to be shown when view state is set to MAIN */}
+          {viewState === VIEW_STATES.MAIN && (
+            <Button
+              className={`${$button.default} ${$home.bold}`}
+              variant="outlined"
+              sx={{
+                color: "#5347d7",
+                borderColor: "#5347d7",
+                borderRadius: "8px",
+                fontFamily: "Quicksand",
+                fontSize: "1em",
+                textTransform: "capitalize",
+              }}
+              onClick={handleEditState}
+            >
+              Edit
+            </Button>
+          )}
+          {/* Save and Cancel buttons only to be shown when view state is set to EDIT */}
+          {viewState === VIEW_STATES.EDIT && (
+            <Box className={$StaffPage.displayBoxGap}>
+              <Button
+                onClick={handleMainState}
+                className={`${$button.default} ${$home.bold}`}
+                variant="outlined"
+                sx={{
+                  color: "#5347d7",
+                  borderColor: "#5347d7",
+                  borderRadius: "8px",
+                  fontFamily: "Quicksand",
+                  textTransform: "capitalize",
+                  fontSize: "1em",
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                className={`${$button.default} ${$home.bold}`}
+                sx={[
+                  {
+                    backgroundColor: "#5347d7",
+                    borderRadius: "8px",
+                    border: "none",
+                    color: "#ffffff",
+                    fontFamily: "Quicksand",
+                    textTransform: "capitalize",
+                    fontSize: "1em",
+                  },
+                  buttonSX,
+                ]}
+                type="submit"
+                form="edit"
+                variant="contained"
+              >
+                Save
+              </Button>
+            </Box>
+          )}
+        </Box>
+
+        {/* if view state is "EDIT" then show the edit version of the student page */}
+        {viewState === VIEW_STATES.EDIT && <h3>Edit Profile</h3>}
+
+        {viewState === VIEW_STATES.MAIN && (
+          <Box className={$StaffPage.displayBox}>
+            <Box gap={10} className={$StaffPage.infoBox}>
+              <div className={$StaffPage.singleInfoArea}>
+                <p>Email</p>
+                <p className={$StaffPage.centerText}>{para?.email}</p>
+              </div>
+              <div className={$StaffPage.singleInfoArea}></div>
+            </Box>
+          </Box>
+        )}
+      </Container>
+
+      {viewState === VIEW_STATES.EDIT && (
+        <Stack gap={0.5} sx={{ width: "100%" }}>
+          <form
+            className={$StaffPage.editForm}
+            id="edit"
+            onSubmit={() => alert("Edit Staff placeholder")}
+          >
+            <Stack gap={0.5}>
+              <Container
+                className={$StaffPage.studentEditContainer}
+                sx={{
+                  display: "grid",
+                  gridTemplateColumns: "200px 30px 300px",
+                }}
+              >
+                <label>First Name</label>
+                <p>:</p>
+                <input
+                  type="text"
+                  name="firstName"
+                  placeholder={para?.first_name || ""}
+                  required
+                />
+              </Container>
+              <Container
+                className={$StaffPage.studentEditContainer}
+                sx={{
+                  display: "grid",
+                  gridTemplateColumns: "200px 30px 300px",
+                }}
+              >
+                <label>Last Name</label>
+                <p>:</p>
+                <input
+                  type="text"
+                  name="lastName"
+                  placeholder={para?.last_name || ""}
+                  required
+                />
+              </Container>
+              <Container
+                className={$StaffPage.studentEditContainer}
+                sx={{
+                  display: "grid",
+                  gridTemplateColumns: "200px 30px 300px",
+                }}
+              >
+                <label>Email</label>
+                <p>:</p>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder={para?.email || ""}
+                  required
+                />
+              </Container>
+            </Stack>
+          </form>
+
+          <Container sx={{ marginTop: "2rem" }}>
+            <Box textAlign="center">
+              <Button
+                onClick={() => setArchiveParaPrompt(true)}
+                className={`${$button.default} ${$home.bold}`}
+                variant="outlined"
+                sx={{
+                  color: "#5347d7",
+                  borderColor: "#5347d7",
+                  borderRadius: "8px",
+                  fontFamily: "Quicksand",
+                  textTransform: "capitalize",
+                  fontSize: "1em",
+                }}
+              >
+                Archive {para?.first_name} {para?.last_name}
+              </Button>
+            </Box>
+          </Container>
+        </Stack>
       )}
 
-      {unassignParaPrompt ? (
-        <div>
-          <p>
-            {`Are you sure you want to unassign ${para.first_name} ${para.last_name}?`}
+      {/* Archiving Staff Modal appears when "Archive" button is pressed*/}
+      <Modal
+        open={archiveParaPrompt}
+        onClose={() => setArchiveParaPrompt(false)}
+        aria-labelledby="archiving-student"
+        aria-describedby="archiving-current-student"
+      >
+        <Box className={$Modal.modalContent}>
+          <p className={$StaffPage.centerText}>
+            Are you sure you want to archive
           </p>
-          <button
-            className={`${$button.default} ${$home.bold}`}
-            onClick={() => handleUnassignPara()}
-          >
-            Yes
-          </button>
-          <button
-            className={`${$button.default} ${$home.bold}`}
-            onClick={() => setUnassignParaPrompt(false)}
-          >
-            No
-          </button>
-        </div>
-      ) : null}
-
-      <Link href={`/staff`}>
-        <p>Return to Staff List</p>
-      </Link>
-    </div>
+          <p className={$StaffPage.centerText}>
+            <b>
+              {para?.first_name} {para?.last_name}
+            </b>
+          </p>
+          <Box className={$StaffPage.archiveOptions}>
+            <button
+              className={`${$button.default} ${$home.bold}`}
+              onClick={() => handleArchivePara()}
+            >
+              Yes
+            </button>
+            <button
+              className={`${$button.default} ${$home.bold}`}
+              onClick={() => setArchiveParaPrompt(false)}
+            >
+              No
+            </button>
+          </Box>
+        </Box>
+      </Modal>
+    </Stack>
   );
 };
 
