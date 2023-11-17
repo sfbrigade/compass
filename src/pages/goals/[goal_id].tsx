18e4428198f4fb555580eb37af3947688e0f956f
Connor Wales <connorwales@gmail.com>
2023-11-16 23:06:28 -0800

    add editing form to [goal_id]

--- src/pages/goals/[goal_id].tsx
+++ src/pages/goals/[goal_id].tsx
@@ -7,6 +7,7 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import List from "@mui/material/List";
 import $GoalPage from "../../styles/GoalPage.module.css";
+import $button from "@/styles/Button.module.css";
 
 // const ACTIVE = "ACTIVE";
 // const COMPLETED = "COMPLETED";
@@ -48,15 +49,32 @@ const ViewGoalPage = () => {
   const router = useRouter();
   const { goal_id } = router.query;
 
-  const { data: goal, isLoading } = trpc.iep.getGoal.useQuery({
-    goal_id: goal_id as string,
-  });
+  const [editGoal, setEditGoal] = useState(false);
+  const [editGoalInput, setEditGoalInput] = useState("");
 
   const [activeTab, setActiveTab] = useState<selectionValue>(
     selectionValue.ACTIVE
   );
 
-  console.log(goal);
+  const { data: goal, isLoading } = trpc.iep.getGoal.useQuery({
+    goal_id: goal_id as string,
+  });
+
+  const showEditGoal = () => {
+    setEditGoal(true);
+    setEditGoalInput(goal?.description || "");
+  };
+
+  const submitEditGoal = () => {
+    // trpc edit
+    setEditGoal(false);
+    setEditGoalInput("");
+  };
+
+  const cancelEditGoal = () => {
+    setEditGoal(false);
+    setEditGoalInput("");
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -83,7 +101,44 @@ const ViewGoalPage = () => {
 
       {/* Goal Description */}
       <Container className={$GoalPage.goalDescriptionContainer}>
-        {goal?.description}
+        {!editGoal && (
+          <>
+            <p>{goal?.description}</p>
+            <button className={$button.default} onClick={showEditGoal}>
+              Edit Goal
+            </button>
+          </>
+        )}
+        {editGoal && (
+          <form style={{ marginTop: "1rem" }} onSubmit={submitEditGoal}>
+            <textarea
+              value={editGoalInput}
+              name="description"
+              onChange={(e) => {
+                setEditGoalInput(e.target.value);
+              }}
+              className={$GoalPage.editGoalFormTextArea}
+            />
+            <Grid
+              container
+              justifyContent="space-around"
+              marginY={2}
+              md={6}
+              marginX="auto"
+            >
+              <Grid item>
+                <button className={$button.secondary} onClick={cancelEditGoal}>
+                  Cancel Changes
+                </button>
+              </Grid>
+              <Grid item>
+                <button className={$button.default} type="submit">
+                  Save Changes
+                </button>
+              </Grid>
+            </Grid>
+          </form>
+        )}
       </Container>
 
       <Grid container justifyContent="space-between">
