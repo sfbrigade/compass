0d7fe111723fc18c52c2dc336e9b9b98e61f638f
Tessa Thornberry <tthornberryclass@gmail.com>
2023-11-18 12:23:08 -0800

    Feat para assignment css (#254)
    * Add duration assigment step and create task on save
    
    Closes https://github.com/sfbrigade/compass/issues/238.
    
    * Fix type issues
    
    * added tests for iep.getSubgoal and iep.assignTaskToParas
    
    * added some brute force css on buttons and benchmarks as placeholders for MUI themes for primary and secondary buttons and comments on future fixes
    
    * made Next button disabled unless at least one Staff member is selected and fixed "focus" state of "Save" button to unfocused until selected
    
    ---------
    
    Co-authored-by: Max Isom <hi@maxisom.me>

--- src/components/subgoal/Subgoal.tsx
+++ src/components/subgoal/Subgoal.tsx
@@ -11,72 +11,63 @@ interface SubgoalProps {
 
 const Subgoals = ({ subgoal }: SubgoalProps) => {
   const [isAssignmentModalOpen, setIsAssignmentModalOpen] = useState(false);
-  const task = trpc.iep.tempAddTaskToSelf.useMutation();
-
-  const assignToPara = async () => {
-    const result = await task.mutateAsync({
-      subgoal_id: subgoal.subgoal_id,
-      due_date: new Date(2023, 8, 20),
-      trial_count: 5,
-    });
-    if (!result) {
-      alert("Error: Benchmark already assigned to self.");
-    } else {
-      alert("Success! Benchmark assigned to self.");
-    }
-  };
 
   return (
-    <>
+    <Box sx={{ border: "1px solid #B9C1C6" }}>
       <Box
         sx={{
-          display: "flex",
+          display: "flex-col",
           justifyContent: "space-between",
-          backgroundColor: "#f4d5d5",
+          backgroundColor: "#fff",
           padding: "1rem",
         }}
       >
         <p>{subgoal.description}</p>
-        <Button
+        <Box
           sx={{
-            height: "24px",
-            width: "auto",
-            padding: "0px 20px",
-            backgroundColor: "#5347d7",
-            borderRadius: "5px",
-            border: "none",
-            color: "#ffffff",
-            fontFamily: "Quicksand",
+            display: "flex",
+            justifyContent: "space-between",
           }}
-          variant="contained"
-          onClick={assignToPara}
         >
-          Assign
-        </Button>
-
-        <Button
-          sx={{
-            height: "24px",
-            width: "auto",
-            padding: "0px 20px",
-            backgroundColor: "#5347d7",
-            borderRadius: "5px",
-            border: "none",
-            color: "#ffffff",
-            fontFamily: "Quicksand",
-          }}
-          variant="contained"
-          onClick={() => setIsAssignmentModalOpen(true)}
-        >
-          [Draft] Assign
-        </Button>
+          <p> BASELINE</p>
+          <p> CURRENT</p>
+          <Box
+            sx={{
+              display: "flex-col",
+            }}
+          >
+            <p>STAFF</p>
+            <Button
+              sx={{
+                height: "24px",
+                flex: "flex-end",
+                width: "auto",
+                padding: "20px 10px",
+                backgroundColor: "#fff ",
+                borderWidth: "1px",
+                borderColor: "#20159E",
+                borderRadius: "8px",
+                color: "#20159E",
+                fontFamily: "Quicksand",
+                textTransform: "none",
+                "&:hover": {
+                  backgroundColor: "#F6F5FF",
+                },
+              }}
+              variant="outlined"
+              onClick={() => setIsAssignmentModalOpen(true)}
+            >
+              Assign Staff
+            </Button>
+          </Box>
+        </Box>
       </Box>
       <SubgoalAssignmentModal
         isOpen={isAssignmentModalOpen}
         onClose={() => setIsAssignmentModalOpen(false)}
         subgoal_id={subgoal.subgoal_id}
       />
-    </>
+    </Box>
   );
 };
 
