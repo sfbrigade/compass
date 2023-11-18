53fca3e95308f8705335ff5e80bf8e99ee31b197
Francis Li <mail@francisli.com>
2023-11-18 13:24:03 -0800

    Add config for static files, next-auth mock

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,14 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
+  staticDirs: ["../public"],
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@tomfreudenberg/next-auth-mock/storybook",
   ],
   framework: {
     name: "@storybook/nextjs",
