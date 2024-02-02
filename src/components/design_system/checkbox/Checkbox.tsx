0f58f6dd6bd9dc90631fab3acc7c36ef1daeca2d
Tessa Thornberry <tthornberryclass@gmail.com>
2024-02-01 16:58:26 -0800

    working on customizing CSS for MUI checkboxes

--- src/components/design_system/checkbox/Checkbox.tsx
+++ src/components/design_system/checkbox/Checkbox.tsx
@@ -6,4 +6,21 @@
  * 5) Push code to branch and do a PR referencing the specific issue task you took for issue # 255.
  * NOTE: If you want a css.d.ts file to be generated or updated to help with any type issues, you can run `npm run type-css`
  * */
-export {};
+
+import { Checkbox } from "@mui/material";
+import { alpha, styled } from "@mui/material";
+// import React from "react";
+import $checkbox from "./Checkbox.module.css";
+
+const DSCheckbox = styled(Checkbox)(() => ({
+  color: "#20159e",
+  "&.Mui-indeterminate": {
+    backgroundColor: "#c2bdf9",
+  },
+  "&.Mui-pressed": {},
+  "&.Mui-checked": {
+    color: "#20159e",
+  },
+}));
+
+export default DSCheckbox;
