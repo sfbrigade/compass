2363f2ff5aebbe50f04d7a42efabc4ee7ec706ae
Hieu Ngo <hieu.ngo12989@gmail.com>
2024-02-15 14:34:05 -0800

    Create reusable template for dropdown menu

--- src/components/design_system/dropdown/Dropdown.tsx
+++ src/components/design_system/dropdown/Dropdown.tsx
@@ -6,4 +6,56 @@
  * 5) Push code to branch and do a PR referencing the specific issue task you took for issue # 255.
  * NOTE: If you want a css.d.ts file to be generated or updated to help with any type issues, you can run `npm run type-css`
  * */
-export {};
+// export {};
+
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useState } from "react";
+
+// Both are strings to simplify the data processing.
+// If numbers are used, use them as strings instead.
+interface MenuItemProp {
+  value: string;
+  label: string;
+}
+
+// optional labels and classNames for customizations
+interface DropdownProps {
+  selectItems: MenuItemProp[];
+  label?: string;
+  className?: string;
+}
+
+const Dropdown = ({ selectItems, label, className }: DropdownProps) => {
+  const [age, setAge] = useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120, maxWidth: "fit-content" }} className={className}>
+      <FormControl fullWidth>
+        <InputLabel id="dropdown-label">{label}</InputLabel>
+        <Select
+          labelId="dropdown-label"
+          id="dropdown-select"
+          value={age}
+          label={label}
+          onChange={handleChange}
+        >
+          {selectItems?.map((item) => (
+            <MenuItem key={item.value} value={item.value}>
+              {item.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default Dropdown;
