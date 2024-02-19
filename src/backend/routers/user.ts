810e783b090ecb9258ea16dfb300be6a820c3b91
amantri <829820+amantri@users.noreply.github.com>
2024-02-19 13:12:15 -0800

    Add a 'isCaseManager' function to user
    Fix #222

--- src/backend/routers/user.ts
+++ src/backend/routers/user.ts
@@ -19,4 +19,19 @@ export const user = router({
 
     return user;
   }),
+
+  /**
+   * @returns Whether the current user is a case manager
+   */
+  isCaseManager: authenticatedProcedure.query(async (req) => {
+    const { userId } = req.ctx.auth;
+
+    const result = await req.ctx.db
+      .selectFrom("paras_assigned_to_case_manager")
+      .select("case_manager_id")
+      .where("case_manager_id", "=", userId)
+      .execute();
+
+    return result.length > 0;
+  }),
 });
