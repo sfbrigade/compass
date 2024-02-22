08d003fe9db878874f147073aa01df991270d246
Hieu Ngo <hieu.ngo12989@gmail.com>
2024-02-21 18:42:31 -0800

    Add CSS to Dropdown component. Restore settings page to original.

--- src/components/design_system/dropdown/Dropdown.tsx
+++ src/components/design_system/dropdown/Dropdown.tsx
@@ -3,6 +3,7 @@ import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
+import $dropdown from "../dropdown/Dropdown.module.css";
 
 // Both are strings to simplify the data processing.
 // If numbers are used, use them as strings instead.
@@ -12,14 +13,15 @@ interface MenuitemClass {
 }
 
 // selectedOption states are needed to define the chosen item
-// optional labels and classNames for customizations
+// [OPTIONAL] labels, classNames, disable for customizations
 interface DropdownProps {
   itemList: MenuitemClass[];
   selectedOption: string;
   setSelectedOption: React.Dispatch<React.SetStateAction<string>>;
   label?: string;
   className?: string;
-  disabled?: boolean;
+  formDisabled?: boolean;
+  optionDisabled?: string[];
 }
 
 const Dropdown = ({
@@ -28,13 +30,15 @@ const Dropdown = ({
   setSelectedOption,
   label,
   className,
-  disabled,
+  formDisabled,
+  optionDisabled,
 }: DropdownProps) => {
   const handleChange = (event: SelectChangeEvent) => {
     setSelectedOption(event.target.value);
   };
 
   return (
+    // Minimum styles used. More can be defined in className.
     <Box sx={{ minWidth: 120, maxWidth: "fit-content" }} className={className}>
       <FormControl fullWidth>
         <InputLabel id="dropdown-label">{label}</InputLabel>
@@ -44,10 +48,19 @@ const Dropdown = ({
           value={selectedOption}
           label={label}
           onChange={handleChange}
-          disabled={disabled}
+          // Allow disabling of form
+          disabled={formDisabled}
         >
           {itemList?.map((item) => (
-            <MenuItem key={item.value} value={item.value}>
+            <MenuItem
+              key={item.value}
+              value={item.value}
+              className={`${
+                selectedOption === item.value ? $dropdown.selected : ""
+              } ${$dropdown.default}`}
+              // Allow disabling of named keys used as an array of strings
+              disabled={optionDisabled?.includes(item.value)}
+            >
               {item.label}
             </MenuItem>
           ))}
