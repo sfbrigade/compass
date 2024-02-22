fe5502a1a26920c9d825d521c2256771babd3b98
Hieu Ngo <hieu.ngo12989@gmail.com>
2024-02-21 17:19:37 -0800

    Extract selectedOption and add disabled property to use as needed

--- src/components/design_system/dropdown/Dropdown.tsx
+++ src/components/design_system/dropdown/Dropdown.tsx
@@ -1,39 +1,37 @@
-/** DESIGN SYSTEM COMPONENT PLACEHOLDER
- * 1) Make a local branch for organizing your component (e.g. "design-systems-button")
- * 2) Replace this file and the corresponding css file(s) with your component file(s), cleaning up any duplicate files that are outside of the design components folder.
- * 3) Search and find all use cases for your component (likely linting will tell you where they are) and update the import paths
- * 4) Check code for errors and delete this comment
- * 5) Push code to branch and do a PR referencing the specific issue task you took for issue # 255.
- * NOTE: If you want a css.d.ts file to be generated or updated to help with any type issues, you can run `npm run type-css`
- * */
-// export {};
-
 import Box from "@mui/material/Box";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
-import { useState } from "react";
 
 // Both are strings to simplify the data processing.
 // If numbers are used, use them as strings instead.
-interface MenuItemProp {
+interface MenuitemClass {
   value: string;
   label: string;
 }
 
+// selectedOption states are needed to define the chosen item
 // optional labels and classNames for customizations
 interface DropdownProps {
-  selectItems: MenuItemProp[];
+  itemList: MenuitemClass[];
+  selectedOption: string;
+  setSelectedOption: React.Dispatch<React.SetStateAction<string>>;
   label?: string;
   className?: string;
+  disabled?: boolean;
 }
 
-const Dropdown = ({ selectItems, label, className }: DropdownProps) => {
-  const [age, setAge] = useState("");
-
+const Dropdown = ({
+  itemList,
+  selectedOption,
+  setSelectedOption,
+  label,
+  className,
+  disabled,
+}: DropdownProps) => {
   const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value);
+    setSelectedOption(event.target.value);
   };
 
   return (
@@ -43,11 +41,12 @@ const Dropdown = ({ selectItems, label, className }: DropdownProps) => {
         <Select
           labelId="dropdown-label"
           id="dropdown-select"
-          value={age}
+          value={selectedOption}
           label={label}
           onChange={handleChange}
+          disabled={disabled}
         >
-          {selectItems?.map((item) => (
+          {itemList?.map((item) => (
             <MenuItem key={item.value} value={item.value}>
               {item.label}
             </MenuItem>
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,10 +1,50 @@
 import React from "react";
+import Dropdown from "../components/design_system/dropdown/Dropdown";
+import $dropdown from "../components/design_system/dropdown/Dropdown.module.css";
+import { useState, useEffect } from "react";
 
 const Settings = () => {
   // TODO? make sure settings displayed reflect the specific logged-in user
+  const [selectedOption, setSelectedOption] = useState("1");
+
+  // useEffect(() => {
+  //   console.log('selected Option ', selectedOption);
+  // }, [selectedOption, setSelectedOption])
+
   return (
     <div>
       <p>🚧 Under Construction! 🚧</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Dropdown
+        itemList={[
+          { value: "1", label: "Until a target date has been reached" },
+          {
+            value: "2",
+            label: "Until a number of instances have been recorded",
+          },
+          { value: "3", label: "Until the Para(s) is unassigned" },
+        ]}
+        label="Select option"
+        className={$dropdown.myCustomClass}
+        selectedOption={selectedOption}
+        setSelectedOption={setSelectedOption}
+      />
+      <Dropdown
+        itemList={[
+          { value: "1", label: "All Tasks" },
+          { value: "2", label: "My Tasks" },
+          { value: "3", label: "Completed Tasks" },
+        ]}
+        label="Select option"
+        className={$dropdown.myCustomClass}
+        selectedOption={selectedOption}
+        setSelectedOption={setSelectedOption}
+      />
     </div>
   );
 };
