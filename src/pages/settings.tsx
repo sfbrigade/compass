dc69defcf2e4a7f1199ed119e0e30481654955e1
Hieu Ngo <hieu.ngo12989@gmail.com>
2024-03-15 17:01:43 -0700

    Remove settings changes

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -6,23 +6,6 @@ const Settings = () => {
   return (
     <div>
       <p>🚧 Under Construction! 🚧</p>
-      <h1 style={{ border: "1px solid black" }}>h1 This is h1 Element</h1>
-      <h2 style={{ border: "1px solid black" }}>h2 This is h2 Element</h2>
-      <h3 style={{ border: "1px solid black" }}>h3 This is h3 Element</h3>
-      <h4 style={{ border: "1px solid black" }}>h4 This is h4 Element</h4>
-      <h5 style={{ border: "1px solid black" }}>h5 This is h5 Element</h5>
-      <h6 style={{ border: "1px solid black" }}>h6 This is h6 Element</h6>
-      <p style={{ border: "1px solid black" }}>This is a p Element</p>
-      <p className="body2" style={{ border: "1px solid black" }}>
-        This is a body2 Element
-      </p>
-      <p className="caption" style={{ border: "1px solid black" }}>
-        This is a caption Element
-      </p>
-      <p className="overline" style={{ border: "1px solid black" }}>
-        This is an overline Element
-      </p>
-      <span style={{ border: "1px solid black" }}>This is a Span Element</span>
     </div>
   );
 };
