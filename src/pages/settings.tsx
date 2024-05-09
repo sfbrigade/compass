995dae6455da275ffc39c9643d1a23533434d61d
tessathornberry <tthornberryclass@gmail.com>
2024-05-08 19:47:00 -0700

    added test cehckboxes for code revieww for PR# 321

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import DS_Checkbox from "@/components/design_system/checkbox/Checkbox";
+import React, { useState } from "react";
 
 const Settings = () => {
   // TODO? make sure settings displayed reflect the specific logged-in user
-
+  const [isChecked, setIsChecked] = useState(false);
   return (
     <div>
       <p>🚧 Under Construction! 🚧</p>
+      <DS_Checkbox
+        onClickAction={() => setIsChecked(!isChecked)}
+        text={`This is a Test Checkbox`}
+        checked={isChecked}
+      />
+      <DS_Checkbox
+        onClickAction={() => null}
+        text={`This is a Disabled Checkbox`}
+        checked={false}
+        disabled
+      />
     </div>
   );
 };
