5a5b6cabcb44267d71640210332906de4af64e12
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-07-23 18:27:48 -0700

    feat(update-benchmark-flow): Implement state holding for create page by creating useState object and type, then adding onChange handlers to all TextField elements. Have data persist in each TextField using ternary in value attribute for each. Remove old example Stepper in settings. Add ChangeEvent and FormEvent types to global types.

--- src/pages/students/[student_id]/goals/[goal_id]/create.tsx
+++ src/pages/students/[student_id]/goals/[goal_id]/create.tsx
@@ -1,17 +1,18 @@
 import { trpc } from "@/client/lib/trpc";
+import $button from "@/components/design_system/button/Button.module.css";
 import { GoalHeader } from "@/components/goal-header/goal-header";
+import { ChangeEvent } from "@/types/global";
+import { CheckCircle, TripOriginRounded } from "@mui/icons-material";
 import {
   Box,
   Stack,
-  TextField,
-  Typography,
-  Stepper,
   Step,
   StepLabel,
+  Stepper,
+  TextField,
+  Typography,
 } from "@mui/material";
-import { CheckCircle, TripOriginRounded } from "@mui/icons-material";
 import { useRouter } from "next/router";
-import $button from "@/components/design_system/button/Button.module.css";
 import { useState } from "react";
 
 interface Benchmark {
@@ -20,6 +21,10 @@ interface Benchmark {
   label: string;
   name: string;
   placeholder: string;
+}
+
+interface BenchmarkFormState {
+  [key: string]: string | number | undefined;
 }
 
 const CreateBenchmarkPage = () => {
@@ -38,29 +43,54 @@ const CreateBenchmarkPage = () => {
 
   const [viewState, setViewState] = useState(VIEW_STATES.BENCHMARK_PG_1);
 
-  const steps = ["Instructional Guidelines", "Data Collection"];
+  const [benchmarkFormState, setBenchmarkFormState] =
+    useState<BenchmarkFormState>({
+      description: undefined,
+      setup: undefined,
+      materials: undefined,
+      instructions: undefined,
+      baseline_level: 0,
+      target_level: 0,
+      attempts_per_trial: 0,
+      number_of_trials: 0,
+    });
 
-  console.log(
-    "This is the [goal_id].create.tsx page, which is trigged by clicking the add benchmark button."
-  );
+  const steps = ["Instructional Guidelines", "Data Collection Guidelines"];
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const form = e.currentTarget as HTMLFormElement;
-    const formData = new FormData(form);
+    // const form = e.currentTarget as HTMLFormElement;
+    // const formData = new FormData(form);
+    // console.log("form data", formData);
+
+    // await addSubgoalMutation.mutateAsync({
+    //   goal_id: router.query.goal_id as string,
+    //   status: "In Progress",
+    //   description: formData.get("description") as string,
+    //   setup: formData.get("setup") as string,
+    //   instructions: formData.get("instructions") as string,
+    //   materials: formData.get("materials") as string,
+    //   target_level: Number(formData.get("target_level")),
+    //   baseline_level: Number(formData.get("baseline_level")),
+    //   metric_name: formData.get("metric_name") as string,
+    //   attempts_per_trial: Number(formData.get("attempts_per_trial")) || null,
+    //   number_of_trials: Number(formData.get("number_of_trials")) || null,
+    // });
+    console.log("benchmarkFormState", benchmarkFormState);
     await addSubgoalMutation.mutateAsync({
       goal_id: router.query.goal_id as string,
       status: "In Progress",
-      description: formData.get("description") as string,
-      setup: formData.get("setup") as string,
-      instructions: formData.get("instructions") as string,
-      materials: formData.get("materials") as string,
-      target_level: Number(formData.get("target_level")),
-      baseline_level: Number(formData.get("baseline_level")),
-      metric_name: formData.get("metric_name") as string,
-      attempts_per_trial: Number(formData.get("attempts_per_trial")) || null,
-      number_of_trials: Number(formData.get("number_of_trials")) || null,
+      description: benchmarkFormState["description"] as string,
+      setup: benchmarkFormState["setup"] as string,
+      instructions: benchmarkFormState["instructions"] as string,
+      materials: benchmarkFormState["materials"] as string,
+      target_level: benchmarkFormState["target_level"] as number,
+      baseline_level: benchmarkFormState["baseline_level"] as number,
+      metric_name: "" as string,
+      attempts_per_trial: benchmarkFormState["attempts_per_trial"] as number,
+      number_of_trials: benchmarkFormState["number_of_trials"] as number,
     });
+
     await router.push(
       `/students/${router.query.student_id as string}/goals/${
         router.query.goal_id as string
@@ -104,17 +134,6 @@ const CreateBenchmarkPage = () => {
     },
   ];
 
-  const textFieldData2 = [
-    {
-      title: "Frequency",
-      description: "How often should data be collected for this benchmark?",
-      label: "Frequency",
-      name: "frequency",
-      placeholder:
-        "Eg. once per week, every Monday, as much as possible, etc...",
-    },
-  ];
-
   const renderTextFields = (textFieldData: Benchmark[]) => {
     return textFieldData.map((field, index) => (
       <Stack spacing={2} width="100%" key={index}>
@@ -127,6 +146,17 @@ const CreateBenchmarkPage = () => {
           multiline
           rows={4}
           name={field.name}
+          value={
+            benchmarkFormState[field.name] !== undefined
+              ? benchmarkFormState[field.name]
+              : null
+          }
+          onChange={(e: ChangeEvent) =>
+            setBenchmarkFormState({
+              ...benchmarkFormState,
+              [field.name]: e.target.value,
+            })
+          }
           placeholder={field.placeholder}
         />
       </Stack>
@@ -189,10 +219,8 @@ const CreateBenchmarkPage = () => {
           {viewState === VIEW_STATES.BENCHMARK_PG_2 && (
             <Stack spacing={4} px={3} pb={3} width={"100%"}>
               <Typography variant="h3">
-                Benchmark #1 - Data Collection
+                Benchmark #1 - Data Collection Guidelines
               </Typography>
-
-              {renderTextFields(textFieldData2)}
 
               <Stack spacing={2}>
                 <Typography variant="h4">Metric to track</Typography>
@@ -203,6 +231,17 @@ const CreateBenchmarkPage = () => {
                       required
                       type="number"
                       name="baseline_level"
+                      value={
+                        benchmarkFormState["baseline_level"] !== undefined
+                          ? benchmarkFormState["baseline_level"]
+                          : null
+                      }
+                      onChange={(e: ChangeEvent) =>
+                        setBenchmarkFormState({
+                          ...benchmarkFormState,
+                          ["baseline_level"]: Number(e.target.value),
+                        })
+                      }
                     />
                     <Typography ml={1}>%</Typography>
                   </Stack>
@@ -212,6 +251,17 @@ const CreateBenchmarkPage = () => {
                       required
                       type="number"
                       name="target_level"
+                      value={
+                        benchmarkFormState["target_level"] !== undefined
+                          ? benchmarkFormState["target_level"]
+                          : null
+                      }
+                      onChange={(e: ChangeEvent) =>
+                        setBenchmarkFormState({
+                          ...benchmarkFormState,
+                          ["target_level"]: Number(e.target.value),
+                        })
+                      }
                     />
                     <Typography ml={1}>%</Typography>
                   </Stack>
@@ -219,11 +269,33 @@ const CreateBenchmarkPage = () => {
                     label="Attempts Per Trial"
                     type="number"
                     name="attempts_per_trial"
+                    value={
+                      benchmarkFormState["attempts_per_trial"] !== undefined
+                        ? benchmarkFormState["attempts_per_trial"]
+                        : null
+                    }
+                    onChange={(e: ChangeEvent) =>
+                      setBenchmarkFormState({
+                        ...benchmarkFormState,
+                        ["attempts_per_trial"]: Number(e.target.value),
+                      })
+                    }
                   />
                   <TextField
                     label="Number of Trials"
                     type="number"
                     name="number_of_trials"
+                    value={
+                      benchmarkFormState["number_of_trials"] !== undefined
+                        ? benchmarkFormState["number_of_trials"]
+                        : null
+                    }
+                    onChange={(e: ChangeEvent) =>
+                      setBenchmarkFormState({
+                        ...benchmarkFormState,
+                        ["number_of_trials"]: Number(e.target.value),
+                      })
+                    }
                   />
                 </Stack>
               </Stack>
--- src/types/global.ts
+++ src/types/global.ts
@@ -2,3 +2,5 @@ import { SelectableForTable } from "zapatos/schema";
 
 export type Goal = SelectableForTable<"goal">;
 export type Subgoal = SelectableForTable<"subgoal">;
+export type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
+export type FormEvent = React.FormEvent<HTMLFormElement>;
