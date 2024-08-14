95dec70e18d753ba0f0ef01c8bacebccf507dc11
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2024-08-13 20:42:57 -0700

    fix type error in theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from "@mui/material";
+import { createTheme, Shadows } from "@mui/material";
 
 const { breakpoints } = createTheme();
 
@@ -30,11 +30,11 @@ export const compassTheme = createTheme({
     "0px 1px 3px 1px rgba(0, 0, 0, 0.15), 0px 1px 2px 0px rgba(0, 0, 0, 0.30)",
     "0px 2px 6px 2px rgba(0, 0, 0, 0.15), 0px 1px 2px 0px rgba(0, 0, 0, 0.30)",
     "0px 1px 3px 0px rgba(0, 0, 0, 0.30), 0px 4px 8px 3px rgba(0, 0, 0, 0.15)",
-    "0px 2px 3px 0px rgba(0, 0, 0, 0.30), 0px 6px 10px 4px rgba(0, 0, 0, 0.15)",
+    "0px 2px 3px 0px rgba(141, 59, 59, 0.3), 0px 6px 10px 4px rgba(0, 0, 0, 0.15)",
     "0px 4px 4px 0px rgba(0, 0, 0, 0.30), 0px 8px 12px 6px rgba(0, 0, 0, 0.15)",
-    ...new Array(20).map(() => "none"),
+    ...new Array(19).map(() => "none"),
     // type requires 20+ elements
-  ],
+  ] as Shadows,
   typography: {
     allVariants: {
       letterSpacing: "normal",
