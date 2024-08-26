7cb160aa89f41cabdfef016da69784dbe32f1ab0
Michael Rabbitt <284825+mrabbitt@users.noreply.github.com>
2024-08-26 15:02:51 -0700

    WIP: Mocking for trpc not working yet

--- jest.config.ts
+++ jest.config.ts
@@ -97,10 +97,10 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {
-  //   // Should be kept in sync with "paths" in tsconfig.json
-  //   "@/*": ["src/*"]
-  // },
+  moduleNameMapper: {
+    // Should be kept in sync with "paths" in tsconfig.json
+    "^@/(.+)": ["<rootDir>/src/$1"],
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
--- src/components/design_system/breadcrumbs/Breadcrumbs.test.tsx
+++ src/components/design_system/breadcrumbs/Breadcrumbs.test.tsx
@@ -1,12 +1,42 @@
 import "@testing-library/jest-dom";
-import { describe, expect, jest, test } from "@jest/globals";
+import { describe, expect, jest, test, beforeEach } from "@jest/globals";
 import { render } from "@testing-library/react";
 import mockRouter from "next-router-mock";
+
+// Based on: https://github.com/trpc/trpc/discussions/3612#discussion-4754448
+jest.mock("@/client/lib/trpc", () => {
+  return {
+    trpc: {
+      student: {
+        getStudentById: {
+          useQuery: jest.fn(() => {
+            return jest.fn();
+          }),
+        },
+      },
+      para: {
+        getParaById: {
+          useQuery: jest.fn(() => {
+            return jest.fn();
+          }),
+        },
+      },
+    },
+  };
+});
+
 import BreadcrumbsNav from "./Breadcrumbs";
+import { trpc } from "@/client/lib/trpc";
 
 describe("BreadcrumbsNav", () => {
   test.failing("renders empty for /", async () => {
     await mockRouter.push("/");
+    (trpc.student.getStudentById.useQuery as jest.Mock).mockReturnValueOnce({
+      data: {},
+    });
+    (trpc.para.getParaById.useQuery as jest.Mock).mockReturnValueOnce({
+      data: {},
+    });
 
     const breadcrumbComponent = render(<BreadcrumbsNav />);
 
