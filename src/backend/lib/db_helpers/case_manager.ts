fa8733790c79a8d25025405f9b0829c6de072ada
Katrina Connors <koktakatrina@gmail.com>
2024-10-10 12:21:25 -0700

    fix line break typos

--- src/backend/lib/db_helpers/case_manager.ts
+++ src/backend/lib/db_helpers/case_manager.ts
@@ -70,15 +70,15 @@ export async function sendInviteEmail(
     to: toEmail,
     subject: `Welcome to ${caseManagerName}'s classroom`,
     text: `${first_name}, get set up for data collection with Compass`,
-    html: `Hi ${first_name}! </br>
-${caseManagerName} has added you as a staff member for their classroom in Compass. </br>
-Compass is an all in one tool for collecting data for students’ IEP goals and empowering your classroom team to better assist students. </br>
-How does Compass work? </br>
-Compass will help you organize data collection for the students you work with and securely store the data you collect. </br>
+    html: `Hi ${first_name}! <br/>
+${caseManagerName} has added you as a staff member for their classroom in Compass. <br/>
+Compass is an all in one tool for collecting data for students’ IEP goals and empowering your classroom team to better assist students. <br/>
+How does Compass work? <br/>
+Compass will help you organize data collection for the students you work with and securely store the data you collect. <br/>
 ${caseManagerName} will add you to data collection tasks for specific student goals. Upon logging in, you’ll see which students you’re expected to collect data for. 
-Instructions from ${caseManagerName} will be available with each assignment. When you’re ready to begin, you’ll be able to collect and submit data and notes directly in the app. </br>
-Getting started </br>
-To get set up with Compass, use the link below and log in with the email address you received this message at. </br>
+Instructions from ${caseManagerName} will be available with each assignment. When you’re ready to begin, you’ll be able to collect and submit data and notes directly in the app. <br/>
+Getting started <br/>
+To get set up with Compass, use the link below and log in with the email address you received this message at. <br/>
 Thank you for the key role you play in improving student outcomes!`,
   });
   return;
