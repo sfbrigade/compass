070e95340c098bddd494e3a049b934ac40ed3467
Vincent Shuali <vincent.shuali@gmail.com>
2024-10-18 15:01:16 -0700

    Remove comment, code is self-explanatory

--- src/backend/routers/iep.ts
+++ src/backend/routers/iep.ts
@@ -135,7 +135,6 @@ export const iep = router({
     .mutation(async (req) => {
       const { subgoal_id, assignee_id, due_date, trial_count } = req.input;
 
-      // make sure that this goal belongs to this case manager
       const existingTask = await req.ctx.db
         .selectFrom("task")
         .where("subgoal_id", "=", subgoal_id)
