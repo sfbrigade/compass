8efec6b1941463c985f0f471ab2fd6becf812347
canjalal <vincent.shuali@gmail.com>
2024-11-18 16:39:11 -0600

    Remove deprecated argTypesRegex parameter

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -8,7 +8,6 @@ import "../src/styles/globals.css";
 
 const preview: Preview = {
   parameters: {
-    // actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
