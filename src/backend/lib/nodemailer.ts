7c95a6bb2d15721a3ef3da824749947f84480e59
Francis Li <mail@francisli.com>
2025-03-18 20:00:48 -0700

    [Closes #501] Set mailer SMTP credentials from AWS SES credentials when provided

--- src/backend/lib/nodemailer.ts
+++ src/backend/lib/nodemailer.ts
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 import { createTransport } from "nodemailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 import Email from "email-templates";
@@ -9,6 +10,39 @@ import { fileURLToPath } from "node:url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const getTransporter = (env: Env) => {
+  if (env.SES_REGION && env.SES_ACCESS_KEY_ID && env.SES_SECRET_ACCESS_KEY) {
+    env.EMAIL_SERVICE = "smtp";
+    env.EMAIL_HOST = `email-smtp.${env.SES_REGION}.amazonaws.com`;
+    env.EMAIL_PORT = "587";
+    env.EMAIL_AUTH_USER = env.SES_ACCESS_KEY_ID;
+    // convert secret access key to SMTP password
+    // based on pseudocode at: https://docs.aws.amazon.com/ses/latest/dg/smtp-credentials.html#smtp-credentials-convert
+    const date = "11111111";
+    const service = "ses";
+    const terminal = "aws4_request";
+    const message = "SendRawEmail";
+    const version = 4;
+    let signature;
+    signature = crypto
+      .createHmac("sha256", `AWS4${env.SES_SECRET_ACCESS_KEY}`, {
+        encoding: "utf8",
+      })
+      .update(date)
+      .digest();
+    signature = crypto
+      .createHmac("sha256", signature)
+      .update(env.SES_REGION)
+      .digest();
+    signature = crypto.createHmac("sha256", signature).update(service).digest();
+    signature = crypto
+      .createHmac("sha256", signature)
+      .update(terminal)
+      .digest();
+    signature = crypto.createHmac("sha256", signature).update(message).digest();
+    signature = Buffer.concat([new Uint8Array([version]), signature]);
+    env.EMAIL_AUTH_PASS = signature.toString("base64");
+  }
+
   const options: SMTPTransport.Options = {
     service: env.EMAIL_SERVICE,
     auth: {
--- src/backend/lib/types/env.ts
+++ src/backend/lib/types/env.ts
@@ -6,6 +6,9 @@ export interface Env {
   S3_USER_UPLOADS_ACCESS_KEY_ID: string;
   S3_USER_UPLOADS_SECRET_ACCESS_KEY: string;
   S3_USER_UPLOADS_BUCKET_NAME: string;
+  SES_ACCESS_KEY_ID: string;
+  SES_SECRET_ACCESS_KEY: string;
+  SES_REGION: string;
   EMAIL_SERVICE: string;
   EMAIL_AUTH_USER: string;
   EMAIL_AUTH_PASS: string;
