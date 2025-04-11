2fe02536a5f6358cdc47599bf990483b420d3c46
Vincent Shuali <vincent.shuali@gmail.com>
2025-04-10 21:26:56 -0700

    Add a goal header, and allow it to be non-editable

--- src/pages/students/[student_id]/goals/[goal_id]/benchmarks/[benchmark_id]/view.tsx
+++ src/pages/students/[student_id]/goals/[goal_id]/benchmarks/[benchmark_id]/view.tsx
@@ -2,17 +2,25 @@ import { useRouter } from "next/router";
 import { LineChart } from "@mui/x-charts/LineChart";
 import { trpc } from "@/client/lib/trpc";
 import { calculateSuccessRate } from "@/utils";
+import { GoalHeader } from "@/components/goal-header/goal-header";
 
 const ViewBenchmarkPage = () => {
   const router = useRouter();
 
   const benchmark_id = router.query?.benchmark_id as string;
 
+  const goal_id = router.query?.goal_id as string;
+
+  const { data: goal } = trpc.iep.getGoal.useQuery(
+    { goal_id: goal_id },
+    { enabled: Boolean(goal_id) }
+  );
+
   const {
     data: benchmark,
-    id,
-    isLoading: benchmarkIsLoading,
-    isError,
+    // id,
+    // isLoading: benchmarkIsLoading,
+    // isError,
   } = trpc.iep.getBenchmarkAndTrialData.useQuery(
     {
       benchmark_id: benchmark_id, // how does this line make sense?
@@ -48,6 +56,15 @@ const ViewBenchmarkPage = () => {
         width={500}
         height={300}
       />
+      {goal && (
+        <GoalHeader
+          name={`Goal #${goal.number}`}
+          description={goal.description}
+          createdAt={goal.created_at}
+          goalId={goal.goal_id}
+          editable={false}
+        />
+      )}
     </div>
   );
 };
