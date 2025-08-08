63772440bfcf302f5369506809717b572f52dded
Francis Li <mail@francisli.com>
2025-08-07 17:05:06 -0700

    Restore original default size of Search box

--- src/components/design_system/search/Search.tsx
+++ src/components/design_system/search/Search.tsx
@@ -12,6 +12,7 @@ const Search = ({ className, ...inputBaseProps }: InputBaseProps) => {
     input: {
       ...inputBaseProps.slotProps?.input,
       className: classes.search__input,
+      size: 8,
     },
   };
   inputBaseProps.startAdornment = inputBaseProps.startAdornment || (
